2782d389ac65193488ce0f35ca4e2ed457f5cfb1
Pranjal Shukla <pranjal779@gmail.com>
2024-06-08 07:39:34 -0400

    Create quiz.py

---
+++ pythonQuiz/6-8-24/quiz.py
@@ -0,0 +1,11 @@
+List = [[1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 16]]
+for i in range(0, 4):
+    print(List[i][1], end='')
+
+# Ouput:
+# 261014
+
+# ** Process exited - Return Code: 0 **
